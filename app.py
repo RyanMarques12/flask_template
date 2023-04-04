c189975099813d43b37e04758793519d7232c4f8
Ryan Marques <100864847+RyanMarques12@users.noreply.github.com>
2023-04-04 14:41:08 -0500

    Updated files

--- app.py
+++ app.py
@@ -3,13 +3,25 @@ from flask import render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/index')
 def index():
     return render_template('index.html')
 
 @app.route('/about')
-def mike():
+def about():
     return render_template('about.html')
+
+@app.route('/base')
+def base():
+    return render_template('base.html')
+
+@app.route('/404')
+def error():
+    return render_template('404.html')
+
+@app.route('/500')
+def error():
+    return render_template('500.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
@@ -18,9 +30,9 @@ if __name__ == '__main__':
 def submit_form():
    
     data = {
-        'name': request.form['name'],
-        'email': request.form['email'],
-        'comments': request.form['comments']
+        'title': request.form['title'],
+        'author': request.form['author'],
+        'pages': request.form['pages']
     }
    
    
